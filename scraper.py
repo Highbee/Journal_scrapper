596ef3f105061387508d5ee017917bf457197ab4
Highbee <Brymo773@gmail.com>
2021-05-06 13:37:57 +0100

    added font, justify and font style support

--- scraper.py
+++ scraper.py
@@ -7,7 +7,7 @@
          probably any aggregator sites using exactly the same HTML template
     ---------------usage-------------
     1. Not yet defined
-    
+
     ---------------dependencies-----------
     1.  BeautifulSoup4==4.9.1
     2.  requests==2.24.0
@@ -17,6 +17,8 @@
 from bs4 import BeautifulSoup
 import requests
 import docx
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 
 listing_url = input("Input listing URL: ").strip()
 
@@ -121,7 +123,13 @@ if response_text != False:
     mydoc = docx.Document()
     # define working document name. Prefix volume+issue details with journal name
     file_name = f"{usable_volume}.docx"
+    # create style
+    style = mydoc.styles['Normal']
+    font = style.font
+    font.name = "Times New Roman"
+    font.size = Pt(11)
     mydoc.save(file_name)
+
     i = 0
     for a in articles:
         usable_title = get_title(a)
@@ -131,10 +139,12 @@ if response_text != False:
         usable_abstract = fetch_abstract(usable_article_url)
 
         mydoc = docx.Document(file_name)
-        paragraph = mydoc.add_paragraph(f"{usable_authors}.")
+        paragraph = mydoc.add_paragraph(
+            f"{usable_authors}.")
         paragraph.add_run(f"{usable_title}.").bold = True
         paragraph.add_run(
             f"{usable_volume}: {usable_page_number}.  {usable_abstract}")
+        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
         mydoc.add_paragraph("")
         mydoc.save(file_name)
         i += 1
