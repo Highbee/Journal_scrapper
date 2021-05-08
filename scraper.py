9836f53c0b4983f978cd24df018fbab7a655a5c7
Highbee <Brymo773@gmail.com>
2021-05-08 05:45:32 +0100

    A major upgrade. The scraper.py script now work with Issue listing URL i.e. the archive! Notes: pages in Paginated archives needs to be treated separately

--- scraper.py
+++ scraper.py
@@ -20,7 +20,37 @@ import docx
 from docx.shared import Pt
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
-listing_url = input("Input listing URL: ").strip()
+issues_listing_url = input("Input Issues listing URL ('Archive'): ").strip()
+
+
+def get_journal_name(journal_name_bsoup):
+    try:
+        journal_name = journal_name_bsoup.select(
+            '#headerNavigationContainer > div > div > div > div:nth-child(1) > a')
+        journal_name = journal_name[0].getText().strip()
+    except IndexError:
+        journal_name = ""
+    return journal_name
+
+
+def get_issue_url(issue_listing_url):
+    try:
+        issue_response_text = get_reponse_text(issue_listing_url)
+
+        issues_soup = create_bsoup(issue_response_text)
+
+        issues_url_list_temp = issues_soup.select(
+            '#pkp_content_main > div.page.page_issue_archive > ul > li > div > a')
+        # print(type(issues_url_list_temp))
+        # print(len(issues_url_list_temp))
+        issues_url_list = [a.get('href').strip()
+                           for a in issues_url_list_temp]
+
+        return issues_url_list
+
+    except IndexError:
+        issues_url_list = False
+        return issues_url_list
 
 
 def get_volume(bsoup):
@@ -83,6 +113,7 @@ def fetch_abstract(url):
                 "#pkp_content_main > div.page.page_article > article > div > div.main_entry > div.item.abstract")
             abstract_temp = abstract[0].getText()
             abstract = abstract_temp
+            # print("First Conditional at work")
             return abstract
         except IndexError:
             try:
@@ -90,6 +121,7 @@ def fetch_abstract(url):
                     "#pkp_content_main > div.page.page_article > article > div > div.main_entry > div.item.abstract p")
                 abstract_temp = "".join([a.getText() for a in abstract])
                 abstract = abstract_temp
+                # print("Second Conditional at work!")
                 return abstract
             except IndexError:
                 abstract = ". No Abstract Found! "
@@ -111,42 +143,52 @@ def create_bsoup(response_text):
     return bsoup
 
 
-response_text = get_reponse_text(listing_url)
-# implementation
-if response_text != False:
-    bsoup = create_bsoup(response_text)
-    articles = bsoup.select(
-        ".obj_article_summary"
-    )
-    usable_volume = get_volume(bsoup)
-    # create blank document with volume name+ issue
-    mydoc = docx.Document()
-    # define working document name. Prefix volume+issue details with journal name
-    file_name = f"{usable_volume}.docx"
-    # create style
-    style = mydoc.styles['Normal']
-    font = style.font
-    font.name = "Times New Roman"
-    font.size = Pt(11)
-    mydoc.save(file_name)
+issues_list = get_issue_url(issues_listing_url)
+issue_i = 0
+for issue in issues_list:
+    response_text = get_reponse_text(issue)
+    # implementation
+    if response_text != False:
+        bsoup = create_bsoup(response_text)
+        articles = bsoup.select(
+            ".obj_article_summary"
+        )
+        usable_volume = get_volume(bsoup)
+        # create blank document
 
-    i = 0
-    for a in articles:
-        usable_title = get_title(a)
-        usable_authors = get_authors(a)
-        usable_page_number = get_page_number(a)
-        usable_article_url = get_article_url(a)
-        usable_abstract = fetch_abstract(usable_article_url)
-
-        mydoc = docx.Document(file_name)
-        paragraph = mydoc.add_paragraph(
-            f"{usable_authors}.")
-        paragraph.add_run(f"{usable_title}.").bold = True
-        paragraph.add_run(
-            f"{usable_volume}: {usable_page_number}.  {usable_abstract}")
-        paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
-        mydoc.add_paragraph("")
+        mydoc = docx.Document()
+        # Save working document name. Prefix journal name +vol+issue details
+        journal_name = get_journal_name(bsoup)
+        file_name = f"{journal_name}_{usable_volume}.docx"
+        file_name = file_name.replace(":", "")
+        print(f"creating file: {file_name}")
+        # create style
+        style = mydoc.styles['Normal']
+        font = style.font
+        font.name = "Times New Roman"
+        font.size = Pt(11)
         mydoc.save(file_name)
-        i += 1
-        print(f"Processed: {i}")
-    print("All complete!!!")
+
+        i = 0
+        for a in articles:
+            usable_title = get_title(a)
+            usable_authors = get_authors(a)
+            usable_page_number = get_page_number(a)
+            usable_article_url = get_article_url(a)
+            usable_abstract = fetch_abstract(usable_article_url)
+
+            mydoc = docx.Document(file_name)
+            paragraph = mydoc.add_paragraph(
+                f"{usable_authors}. ")
+            paragraph.add_run(f"{usable_title}. ").bold = True
+            paragraph.add_run(
+                f"{usable_volume}: {usable_page_number}.  {usable_abstract} ")
+            paragraph.alignment = WD_ALIGN_PARAGRAPH.JUSTIFY
+            mydoc.add_paragraph("")
+            mydoc.save(file_name)
+            i += 1
+            print(f"Articles Processed: {i}")
+        issue_i += 1
+        print(f"No of issues saved: {issue_i}")
+
+print("All issues listed in the page have been saved successfully")
